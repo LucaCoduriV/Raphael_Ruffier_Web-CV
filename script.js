e58a6cabbb7a7d0aedde43c2f8bab95497ef9eb9
Luca Coduri <luca.coduri@cpnv.ch>
2019-04-18 15:54:28 +0200

    Ajout de la sidebar avec animation

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	$('#hamburger').click(function(){
 		$(this).toggleClass('open');
+    $("#sidebar").toggleClass("active");
+    $("#body-container").toggleClass("active");
+
 	});
 });
